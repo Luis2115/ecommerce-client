ada09f006f708430a64ae75838f2d09ab2a73838
Luis2115 <lmorochofebres@gmail.com>
2021-10-24 20:09:45 -0500

    Creando el contenido del Formulario login con sus validaciones con formik y yup

--- components/Auth/LoginForm/LoginForm.js
+++ components/Auth/LoginForm/LoginForm.js
@@ -1,11 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button } from "semantic-ui-react";
+import { Formik, FormikContext, useFormik } from "formik";
+import * as Yup from "yup";
+import { toast } from "react-toastify";
 
 export default function LoginForm(props) {
   const { showRegisterForm } = props;
+
+  //hacemos uso del hooks de formit para validar el formulario
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: (formData) => {
+      console.log(formData);
+    },
+  });
+
   return (
-    <div>
-      <h1>Estamos en el formulario de login</h1>
-      <button onClick={showRegisterForm}>ir al registro</button>
-    </div>
+    <Form className="login-form" onSubmit={formik.handleSubmit}>
+      <Form.Input
+        name="identifier"
+        type="text"
+        placeholder="Correo Electronico"
+        onChange={formik.handleChange}
+        error={formik.errors.identifier}
+      />
+      <Form.Input
+        name="password"
+        type="password"
+        placeholder="Contraseña"
+        onChange={formik.handleChange}
+        error={formik.errors.password}
+      />
+      <div className="actions">
+        <Button type="button" basic onClick={showRegisterForm}>
+          Registrar usuario
+        </Button>
+        <div>
+          <Button className="submit" type="submit">
+            Ingresar
+          </Button>
+          <Button type="button">¿Se te olvidó tu contraseña?</Button>
+        </div>
+      </div>
+    </Form>
   );
+}
+
+//validaciones iniciales
+function initialValues() {
+  return { identifier: "", password: "" };
+}
+
+//Validamos que los campos sean requeridos y su correcto tipo
+function validationSchema() {
+  return {
+    identifier: Yup.string().email(true).required(true),
+    password: Yup.string().required(true),
+  };
 }

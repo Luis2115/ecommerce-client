ee0643a44b01d94ab9664de5edbd143b38e269b8
Luis2115 <lmorochofebres@gmail.com>
2021-10-26 12:46:06 -0500

    Funciones de remover el token y comprobar si ha expirado

--- api/token.js
+++ api/token.js
@@ -1,4 +1,5 @@
 import { TOKEN } from "../utils/constants";
+import jwtDecode from "jwt-decode";
 
 //funcion que guardara el token en el localStorage
 export function setToken(token) {
@@ -8,4 +9,21 @@ export function setToken(token) {
 //funcion que retornara el token almacenado en el localStorage
 export function getToken() {
   return localStorage.getItem(TOKEN);
+}
+
+export function removeToken() {
+  localStorage.removeItem(TOKEN);
+}
+
+//funcion que verificara si el token ha expirado
+export function hasExpiredToken(token) {
+  const tokenDecode = jwtDecode(token);
+  //obtenemos la fecha de expiracion y lo multiplicamos por 1000 milisegundos
+  const expireDate = tokenDecode.exp * 1000;
+  //obtenemos la fecha actual
+  const currentDate = new Date().getTime();
+  if (currentDate > expireDate) {
+    return true;
+  }
+  return false;
 }

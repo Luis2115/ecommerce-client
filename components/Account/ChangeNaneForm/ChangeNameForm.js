59f88389dbc35ba58ad156c40678431382276d1b
Luis2115 <lmorochofebres@gmail.com>
2021-10-27 10:31:17 -0500

    Definimos el diseño del formulario y las validaciones correspondientes

--- components/Account/ChangeNaneForm/ChangeNameForm.js
+++ components/Account/ChangeNaneForm/ChangeNameForm.js
@@ -1,19 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "semantic-ui-react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { toast } from "react-toastify";
+import { updateNameApi } from "../../../api/user";
 
 export default function ChangeNameForm(props) {
-  const { user } = props;
+  const { user, logout, setReloadUser } = props;
+  const [loading, setLoading] = useState(false);
+
+  const formik = useFormik({
+    initialValues: initialValues(user.name, user.lastname),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: async (formData) => {
+      setLoading(true);
+      const response = await updateNameApi(user.id, formData, logout);
+      if (!response) {
+        toast.error("Error al actualizar el Nombre y Apellidos");
+      } else {
+        setReloadUser(true); //actualizamos el estado del menu del usuario para ver los cambios
+        toast.success("Nombre y Apellidos Actualizados ");
+      }
+      setLoading(false);
+    },
+  });
 
   return (
     <div className="change-name-form">
       <h4>Cambia tu nombre y apellidos</h4>
-      <Form>
+      <Form onSubmit={formik.handleSubmit}>
         <Form.Group widths="equal">
-          <Form.Input name="name" placeholder="Nuevo Nombre" />
-          <Form.Input name="lastname" placeholder="Nuevos Apellidos" />
+          <Form.Input
+            name="name"
+            placeholder="Nuevo Nombre"
+            onChange={formik.handleChange}
+            value={formik.values.name}
+            error={formik.errors.name}
+          />
+          <Form.Input
+            name="lastname"
+            placeholder="Nuevos Apellidos"
+            onChange={formik.handleChange}
+            value={formik.values.lastname}
+            error={formik.errors.lastname}
+          />
         </Form.Group>
-        <Button className="submit">Actualizar usuario</Button>
+        <Button className="submit" type="submit" loading={loading}>
+          Actualizar usuario
+        </Button>
       </Form>
     </div>
   );
+}
+
+function initialValues(name, lastname) {
+  return {
+    name: name || "",
+    lastname: lastname || "",
+  };
+}
+
+function validationSchema() {
+  return {
+    name: Yup.string().required(true),
+    lastname: Yup.string().required(true),
+  };
 }

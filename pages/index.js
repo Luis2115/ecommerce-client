a7acc9746a2d7210503b99f6750e74eb30eed6f8
Luis2115 <lmorochofebres@gmail.com>
2021-10-27 02:15:50 -0500

    al BasicLayout le asigamos una clase hija de la clase superior

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import BasicLayout from "../layouts/BasicLayout";
 
 export default function Home() {
   return (
-    <BasicLayout>
+    <BasicLayout className="home">
       <h1>estamos en la home</h1>
     </BasicLayout>
   );

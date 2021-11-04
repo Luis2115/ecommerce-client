cae82dcadb10e4b3948bf2d3b519cb1bec754cee
Luis2115 <lmorochofebres@gmail.com>
2021-11-04 13:10:46 -0500

    Renderizando los productos del Home y su estilos

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,34 @@
+import React, { useState, useEffect } from "react";
+import { size, map } from "lodash";
 import BasicLayout from "../layouts/BasicLayout";
+import { getLastProductApi } from "../api/product";
+import { Loader } from "semantic-ui-react";
+import ListProducts from "../components/ListProducts/ListProducts";
 
 export default function Home() {
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const response = await getLastProductApi(20);
+      if (size(response) > 0) {
+        setProducts(response);
+        //console.log(response);
+      } else {
+        setProducts([]);
+      }
+    })();
+  }, []);
+
   return (
     <BasicLayout className="home">
-      <h1>estamos en la home</h1>
+      {!products && <Loader active>Cargando juegos</Loader>}
+      {products && size(products) === 0 && (
+        <div>
+          <h3>No hay juegos</h3>
+        </div>
+      )}
+      {size(products) > 0 && <ListProducts products={products} />}
     </BasicLayout>
   );
 }

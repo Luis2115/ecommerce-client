d6466870570e4d29908f2ec3331ef9e8e9bbd4e3
Luis2115 <lmorochofebres@gmail.com>
2021-11-04 13:12:17 -0500

    Renderizando los productos por categoria, reutilizando el componente ListProducts

--- pages/products/[category].js
+++ pages/products/[category].js
@@ -1,13 +1,68 @@
-import React from "react";
-import BasicLayout from "../../layouts/BasicLayout";
+import React, { useState, useEffect } from "react";
+import { Loader } from "semantic-ui-react";
 import { useRouter } from "next/router";
+import { size } from "lodash";
+import BasicLayout from "../../layouts/BasicLayout";
+import { getProductsCategoryApi, getTotalCategoryApi } from "../../api/product";
+import ListProducts from "../../components/ListProducts";
+import Pagination from "../../components/Pagination/Pagination";
+
+//limite de productos por pagina
+const limitPerPage = 20;
 
 export default function Category() {
   const { query } = useRouter();
+  const [products, setProducts] = useState(null);
+  const [totalProducts, setTotalProducts] = useState(null);
+
+  //funcion para saber a partir de que producto mostrar en la paginacion
+  const getStartItem = () => {
+    const currentPage = parseInt(query.page);
+    if (!query.page || currentPage === 1) return 0;
+    else return currentPage * limitPerPage - limitPerPage;
+  };
+
+  //efecto que permitira renderizar los productos de cada categoria
+  useEffect(() => {
+    (async () => {
+      if (query.category) {
+        const response = await getProductsCategoryApi(
+          query.category,
+          limitPerPage,
+          getStartItem()
+        );
+        //llenamos el array de productos para renderizar lo
+        setProducts(response);
+        // console.log(products);
+      }
+    })();
+  }, [query]);
+
+  //efecto que nos traera el total de productos por categoria
+  useEffect(() => {
+    (async () => {
+      const response = await getTotalCategoryApi(query.category);
+      //console.log(response);
+      setTotalProducts(response);
+    })();
+  }, [query]);
 
   return (
     <BasicLayout className="category">
-      <h1>Estamos en categoria: {query.category}</h1>
+      {!products && <Loader active>Cargando Productos</Loader>}
+      {products && size(products) === 0 && (
+        <div>
+          <h3>No hay productos</h3>
+        </div>
+      )}
+      {size(products) > 0 && <ListProducts products={products} />}
+      {totalProducts ? (
+        <Pagination
+          totalProducts={totalProducts}
+          page={query.page ? parseInt(query.page) : 1}
+          limitPerPage={limitPerPage}
+        />
+      ) : null}
     </BasicLayout>
   );
 }

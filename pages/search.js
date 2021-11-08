e414c43e8b93085e02bf09bab7df0a81e1d7794b
Luis2115 <lmorochofebres@gmail.com>
2021-11-08 16:52:24 -0500

    creando la pagina del Search donde se renderizaran los productos buscados

---
+++ pages/search.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { Loader } from "semantic-ui-react";
+import { useRouter } from "next/router";
+import { size } from "lodash";
+import BasicLayout from "../layouts/BasicLayout";
+import { searchProductApi } from "../api/product";
+import ListProducts from "../components/ListProducts";
+
+export default function search() {
+  const [products, setProducts] = useState(null);
+  const { query } = useRouter();
+
+  useEffect(() => {
+    document.getElementById("search-product").focus();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      if (size(query.query) > 0) {
+        const response = await searchProductApi(query.query);
+        console.log(response);
+        if (size(response) > 0) setProducts(response);
+        else setProducts([]);
+      } else {
+        setProducts([]);
+      }
+    })();
+  }, [query]);
+
+  return (
+    <BasicLayout className="search">
+      {!products && <Loader active>Cargando Productos</Loader>}
+      {products && size(products) === 0 && (
+        <div>
+          <h3>No se han encontrado productos</h3>
+        </div>
+      )}
+      {size(products) > 0 && <ListProducts products={products} />}
+    </BasicLayout>
+  );
+}

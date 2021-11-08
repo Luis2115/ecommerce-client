c9cfcccb58432ffc26fb50d52c0d417010d8321d
Luis2115 <lmorochofebres@gmail.com>
2021-11-08 16:50:23 -0500

    Programando funcionalidad del Search

--- components/Header/TopBar/TopBar.js
+++ components/Header/TopBar/TopBar.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Grid, Image, Input } from "semantic-ui-react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function TopBar() {
   return (
@@ -30,5 +31,23 @@ function Logo() {
 }
 
 function Search() {
-  return <Input id="search-product" icon={{ name: "search" }}></Input>;
+  const [searchStr, setSearchStr] = useState("");
+  const [load, setLoad] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (load) {
+      router.push(`/search?query=${searchStr}`);
+    }
+    setLoad(true);
+  }, [searchStr]);
+
+  return (
+    <Input
+      id="search-product"
+      icon={{ name: "search" }}
+      value={router.query.query}
+      onChange={(_, data) => setSearchStr(data.value)}
+    />
+  );
 }

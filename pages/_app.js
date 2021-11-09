dd1fdc22e9d4ddfedb794561f30ec8569a84a8b9
Luis2115 <lmorochofebres@gmail.com>
2021-11-09 12:31:45 -0500

    Engloblamos el proyecto con el nuevo contexto del Carrito y creamos las funciones que requiere el context

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect, useMemo } from "react";
-import { ToastContainer } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import jwtDecode from "jwt-decode";
 import { useRouter } from "next/router";
 import AuthContext from "../context/AuthContext";
+import CartContext from "../context/CartContext";
 import { setToken, getToken, removeToken } from "../api/token";
+import {
+  getProductsCart,
+  addProductCart,
+  countProductsCart,
+  removeProductCart,
+} from "../api/cart";
 import "../scss/global.scss";
 import "semantic-ui-css/semantic.min.css";
 import "react-toastify/dist/ReactToastify.css";
@@ -11,6 +18,8 @@ import "react-toastify/dist/ReactToastify.css";
 export default function MyApp({ Component, pageProps }) {
   const [auth, setAuth] = useState(undefined);
   const [reloadUser, setReloadUser] = useState(false);
+  const [totalProductsCart, setTotalProductsCart] = useState(0);
+  const [reloadCart, setReloadCart] = useState(false);
   const router = useRouter();
 
   //verificamos si el usuario esta logeado por medio del token que retorna la funcion getToken
@@ -23,6 +32,12 @@ export default function MyApp({ Component, pageProps }) {
     }
     setReloadUser(false);
   }, [reloadUser]);
+
+  //efecto que asignara el total de productos en el carrito, y se ejecutara cuando el reloadCart cambie o el auth
+  useEffect(() => {
+    setTotalProductsCart(countProductsCart);
+    setReloadCart(false);
+  }, [reloadCart, auth]);
 
   const login = (token) => {
     //guardamos el token en el localStorage mediante la funcion setToken
@@ -38,6 +53,22 @@ export default function MyApp({ Component, pageProps }) {
     }
   };
 
+  //Funcion que se encargara de agregar un producto al carrito si el usuario esta logeado
+  const addProduct = (product) => {
+    const token = getToken();
+    if (token) {
+      addProductCart(product);
+      setReloadCart(true); //refrescamos el contador del carrito mediante un reload
+    } else {
+      toast.warning("Para añadir un producto al carrito debes iniciar sesión");
+    }
+  };
+
+  const removeProduct = (product) => {
+    removeProductCart(product);
+    setReloadCart(true);
+  };
+
   //se hace uso del hook useMemo, el cual nos permitira memorizar los datos que se le ingresen
   const authData = useMemo(
     () => ({
@@ -49,24 +80,38 @@ export default function MyApp({ Component, pageProps }) {
     [auth]
   );
 
+  //se hace uso del hook useMemo, para memorizar los datos del cart
+  const cartData = useMemo(
+    () => ({
+      productsCart: totalProductsCart,
+      addProductCart: (product) => addProduct(product),
+      getProductCart: getProductsCart,
+      removeProductCart: (product) => removeProduct(product),
+      removeAllProductCart: () => null,
+    }),
+    [totalProductsCart]
+  );
+
   //Comprobamos si el useEffect verifico al usuario logeado
   if (auth === undefined) return null;
 
   //se engloba a todo el proyecto con el contexto creado y se le pasa los valores de que contenga authData
   return (
     <AuthContext.Provider value={authData}>
-      <Component {...pageProps} />
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss={false}
-        draggable
-        pauseOnHover
-      />
+      <CartContext.Provider value={cartData}>
+        <Component {...pageProps} />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss={false}
+          draggable
+          pauseOnHover
+        />
+      </CartContext.Provider>
     </AuthContext.Provider>
   );
 }

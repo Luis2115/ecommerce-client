7fa13bb8b50fab97c7f69ae178f945549d883cbf
Luis2115 <lmorochofebres@gmail.com>
2021-11-18 10:53:31 -0500

    Pagina de la orden con sus estilos

---
+++ pages/orders.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import { Grid } from "semantic-ui-react";
+import { size, map } from "lodash";
+import BasicLayout from "../layouts/BasicLayout";
+import { getOrdersApi } from "../api/order";
+import useAuth from "../hooks/useAuth";
+import Order from "../components/Orders/Order/Order";
+
+export default function Orders() {
+  const [orders, setOrders] = useState(null);
+  const { auth, logout } = useAuth();
+
+  useEffect(() => {
+    (async () => {
+      const response = await getOrdersApi(auth.idUser, logout);
+      setOrders(response || []);
+    })();
+  }, []);
+
+  return (
+    <BasicLayout className="orders">
+      <div className="orders__block">
+        <div className="title">Mis Pedidos</div>
+        <div className="data">
+          {size(orders) === 0 ? (
+            <h2 style={{ textAlign: "center" }}>
+              No cuentas con alguna consulta de orden
+            </h2>
+          ) : (
+            <OrderList orders={orders} />
+          )}
+        </div>
+      </div>
+    </BasicLayout>
+  );
+}
+
+function OrderList(props) {
+  const { orders } = props;
+
+  return (
+    <Grid>
+      {map(orders, (order) => (
+        <Grid.Column mobile={16} tablet={6} computer={8} key={order.id}>
+          <Order order={order} />
+        </Grid.Column>
+      ))}
+    </Grid>
+  );
+}

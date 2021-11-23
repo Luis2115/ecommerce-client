c9d5c35007f2bae99e8c306e204117345fe38853
Luis2115 <lmorochofebres@gmail.com>
2021-11-23 11:39:19 -0500

    Creando el componente que permitira realizar SEO a la pagina

---
+++ components/Seo.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Head from "next/head";
+
+export default function Seo(props) {
+  const { title, description } = props;
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta property="description" content={description} />
+    </Head>
+  );
+}
+
+Seo.defaultProps = {
+  title: "Regenda - Tienda Online",
+  description:
+    "Encontraras todos los productos con los que cuente la empresa, con el fin de realizar una consulta de stock",
+};
